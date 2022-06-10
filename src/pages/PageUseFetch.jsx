7584d267a04b01a30ef2fe52989017173be9fa0d
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-10 16:06:00 +0200

    techBooks with useFetch

--- src/pages/PageUseFetch.jsx
+++ src/pages/PageUseFetch.jsx
@@ -5,11 +5,13 @@ const techBooksUrl = 'https://edwardtanguay.netlify.app/share/techBooks.json';
 
 export const PageUseFetch = () => {
 	const [nouns] = useFetch(nounsUrl);
+	const [techBooks] = useFetch(techBooksUrl);
 
 	return (
 		<div className="page_fetch">
 			<h2>useFetch</h2>
 			<p>There are {nouns.length} nouns: {nouns.map(m => m.article + ' ' + m.singular).join(', ')}</p>
+			<p>There are {techBooks.length} tech books: {techBooks.map(m => m.title).join(', ')}</p>
 		</div>
 	)
 }
