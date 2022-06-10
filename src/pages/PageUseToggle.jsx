c8ff5faa9cf16fdc3bdcb1cf522bb7ad7c5b5d72
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-10 15:05:46 +0200

    userIsOnline works as const

--- src/pages/PageUseToggle.jsx
+++ src/pages/PageUseToggle.jsx
@@ -1,8 +1,18 @@
 export const PageUseToggle = () => {
+	
+	const userIsOnline = false;
+
 	return (
 		<div className="page_useToggle">
 			<h2>useToggle</h2>
-			<p>nnn</p>
+			<div className="buttonArea">
+				{userIsOnline ? (
+					<p>User is online.</p>
+				) : (
+						<p>USER IS OFFLINE</p>
+				)}
+				<button>Toggle User Status</button>
+		</div>
 		</div>
 	)
 }
