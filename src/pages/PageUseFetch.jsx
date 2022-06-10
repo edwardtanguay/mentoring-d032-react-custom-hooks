73a6adb489c6aa1e6777f9df119dcbd358d622b6
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-10 15:57:40 +0200

    nouns load without hook

--- src/pages/PageUseFetch.jsx
+++ src/pages/PageUseFetch.jsx
@@ -1,8 +1,21 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const nounsUrl = 'https://edwardtanguay.netlify.app/share/germanNouns.json';
+
 export const PageUseFetch = () => {
+	const [nouns, setNouns] = useState([]);
+	
+	useEffect(() => {
+		(async () => {
+			setNouns((await axios.get(nounsUrl)).data);
+		})();
+	}, []);
+
 	return (
 		<div className="page_fetch">
 			<h2>useFetch</h2>
-			<p>nnn</p>
+			<p>There are {nouns.length} nouns: {nouns.map(m => m.article + ' ' + m.singular).join(', ')}</p>
 		</div>
 	)
 }
